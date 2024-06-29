b10f210696670273d2b4a79122734e1affa4f69b
Winlecodeur <163679684+Winlecodeur@users.noreply.github.com>
2024-06-29 18:28:16 +0200

    Update urls.py

--- IGS/urls.py
+++ IGS/urls.py
@@ -21,8 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.urls')),
-    path('account/', include('account.urls')),
+    path('app/', include('app.urls')),
+    path('', include('account.urls')),
 ]
 if settings.DEBUG :
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
