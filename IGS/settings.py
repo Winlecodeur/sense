7987dc991bef0ebc0fcec7e69085a6f1278392df
Winlecodeur <163679684+Winlecodeur@users.noreply.github.com>
2024-06-29 20:44:07 +0200

    Update settings.py

--- IGS/settings.py
+++ IGS/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-04q)pd5(@7)j1+g@(8)(ym3=-ltell%s6w$)5wfi$w=emn@4-g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG','False') == True
+DEBUG = False
 
 ALLOWED_HOSTS = ['igetsense.onrender.com', 'www.igetsense.onrender.com']
 
@@ -123,14 +123,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-<<<<<<< HEAD
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-=======
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
->>>>>>> dd043457eb249d17b79dcb87460f92dcb450577e
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
