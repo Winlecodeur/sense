a17e719cf4d055805fc6fce2cfc1b4e773d5cd37
unknown <winalfani@gmail.com>
2024-06-29 22:13:16 +0200

    première modification

--- IGS/settings.py
+++ IGS/settings.py
@@ -123,13 +123,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES = [os.path.join(BASE_DIR, 'static')]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-<<<<<<< HEAD
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-=======
+  
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
->>>>>>> dd043457eb249d17b79dcb87460f92dcb450577e
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- account/urls.py
+++ account/urls.py
@@ -3,8 +3,4 @@ from  account import views
 
 urlpatterns= [
     path('',views.login, name='login' ),
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> dd043457eb249d17b79dcb87460f92dcb450577e
