b62ef10487d28c960397bac9634c4b3fb59a342f
Winlecodeur <163679684+Winlecodeur@users.noreply.github.com>
2024-06-30 09:25:49 +0200

    Update settings.py

--- IGS/settings.py
+++ IGS/settings.py
@@ -121,8 +121,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES = [os.path.join(BASE_DIR, 'static')]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR ,'/var/media')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR ,'media')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
